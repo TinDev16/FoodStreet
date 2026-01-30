536d6dd829cf99813f6826e86d6fa23b14160583
TinDev16 <3123411298@sv.sgu.edu.vn>
2026-01-30 15:41:46 +0700

    upload framework

---
+++ FoodStreetMobile/Views/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Maui.Devices.Sensors;
+
+namespace FoodStreetMobile.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        GetUserLocation();
+    }
+
+    async void GetUserLocation()
+    {
+        try
+        {
+            var request = new GeolocationRequest(
+                GeolocationAccuracy.Best,
+                TimeSpan.FromSeconds(10));
+
+            var location = await Geolocation.Default.GetLocationAsync(request);
+
+            if (location != null)
+            {
+                // Gửi tọa độ sang JS
+                await Dispatcher.DispatchAsync(async () =>
+                {
+                    await webView.EvaluateJavaScriptAsync(
+                        $"showUserLocation({location.Latitude}, {location.Longitude});"
+                    );
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Lỗi GPS", ex.Message, "OK");
+        }
+    }
+}
---
+++ FoodStreetWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FoodStreetWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
